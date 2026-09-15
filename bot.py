70ae4ce48940722c86fa62094b4a7c3a577100a9
aryadokhttt87-png <aryadokhttt87@gmail.com>
2026-09-15 18:28:30 +0330

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = os.environ["BOT_TOKEN"]
+
+CHANNEL = "https://t.me/hosseinii12car"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🚘 به کانال خرید و فروش خودرو خوش آمدید!\n\n"
+        "خودروهای داخلی و خارجی\n"
+        "حواله و خودروهای صفر و کم‌کارکرد\n\n"
+        "برای مشاهده آگهی‌ها وارد کانال شوید:\n"
+        f"{CHANNEL}"
+    )
+
+app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
